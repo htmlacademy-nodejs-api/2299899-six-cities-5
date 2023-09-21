6d8cd28aef8bbf8def7ce5d09f68964560eef2a5
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-09-21 17:23:20 +0300

    feat: cli with commands --help, --version, --import

---
+++ src/cli/commands/help.command.ts
@@ -0,0 +1,23 @@
+import chalk from 'chalk';
+import { Command } from './command.interface.js';
+
+export class HelpCommand implements Command {
+  public getName(): string {
+    return '--help';
+  }
+
+  public async execute(..._parameters: string[]): Promise<void> {
+    console.info(
+      chalk.blue(`
+      Программа для подготовки данных для REST API сервера
+      Пример:
+          ${chalk.green('cli.js --<command> [--arguments]')}
+      Команды:
+          ${chalk.green('--version')}:                    # выводит номер версии
+          ${chalk.green('--help')}:                       # печатает этот текст
+          ${chalk.green('--import <path>')}:              # импортирует данные из TSV
+          ${chalk.green('--generate <n> <path> <url>')}:  # генерирует произвольное количество тестовых данных
+    `)
+    );
+  }
+}
