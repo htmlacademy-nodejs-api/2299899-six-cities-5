cc43913c06321b22d66f789f8f0cc6f2e2fc23a7
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-09-25 16:10:52 +0300

    refactor: stream for tsv file write

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -3,7 +3,7 @@ import { MockServerData } from '../../shared/types/mock-server-data.type.js';
 import { Command } from './command.interface.js';
 import chalk from 'chalk';
 import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
-import { appendFile } from 'node:fs/promises';
+import { TSVFileWriter } from '../../shared/libs/file-writer/tsv-file-writer.js';
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
@@ -18,11 +18,10 @@ export class GenerateCommand implements Command {
 
   private async write(filepath: string, offersCount: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
 
     for (let i = 0; i < offersCount; i++) {
-      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
-        encoding: 'utf8',
-      });
+      await tsvFileWriter.write(tsvOfferGenerator.generate());
     }
   }
 
