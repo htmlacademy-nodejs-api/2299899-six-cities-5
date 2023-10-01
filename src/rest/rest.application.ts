67b6c39f5a4d7541b21a38ad8c10b19ba41f5ff5
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-10-01 16:26:41 +0300

    feat: config updated with convict and rest schema

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,14 +1,15 @@
 import { Config } from '../shared/libs/config/config.interface.js';
+import { RestSchema } from '../shared/libs/config/rest.schema.js';
 import { Logger } from '../shared/libs/logger/logger.interface.js';
 
 export class RestApplication {
   constructor(
     private readonly logger: Logger,
-    private readonly config: Config
+    private readonly config: Config<RestSchema>
   ) {}
 
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $REST_PORT: ${this.config.get('REST_PORT')}`);
+    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
   }
 }
