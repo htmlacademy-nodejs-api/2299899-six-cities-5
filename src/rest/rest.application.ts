fd80f4eba87c2e4502c07cf4ca7b622703304976
Вадим Антипов <vadim.v.antipov@gmail.com>
2023-10-01 16:12:45 +0300

    feat: rest app config and dotenv

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,9 +1,14 @@
+import { Config } from '../shared/libs/config/config.interface.js';
 import { Logger } from '../shared/libs/logger/logger.interface.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(
+    private readonly logger: Logger,
+    private readonly config: Config
+  ) {}
 
   public async init() {
     this.logger.info('Application initialization');
+    this.logger.info(`Get value from env $REST_PORT: ${this.config.get('REST_PORT')}`);
   }
 }
